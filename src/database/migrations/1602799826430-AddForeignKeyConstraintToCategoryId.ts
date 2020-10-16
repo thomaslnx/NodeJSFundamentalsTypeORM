b20d734f61a478c5552082c6f08b9a7e5fcce4e7
thomaslnx <marcos.msilva10@gmail.com>
2020-10-16 15:40:03 -0300

    transactions e categories features

---
+++ src/database/migrations/1602799826430-AddForeignKeyConstraintToCategoryId.ts
@@ -0,0 +1,22 @@
+import { MigrationInterface, QueryRunner, TableForeignKey } from 'typeorm';
+
+export default class AddForeignKeyConstraintToCategoryId1602799826430
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createForeignKey(
+      'transactions',
+      new TableForeignKey({
+        name: 'CategoryReference',
+        columnNames: ['category_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'categories',
+        onDelete: 'SET NULL',
+        onUpdate: 'CASCADE',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('transactions', 'CategoryReference');
+  }
+}
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,53 @@
+import { getCustomRepository, getRepository } from 'typeorm';
+
 // import AppError from '../errors/AppError';
+import TransactionsRepository from '../repositories/TransactionsRepository';
 
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
 
+interface Request {
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+  category: string;
+}
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+    const categoryRepository = getRepository(Category);
+
+    let categoryExists = await categoryRepository.findOne({
+      where: {
+        title: category,
+      },
+    });
+
+    if (!categoryExists) {
+      categoryExists = categoryRepository.create({
+        title: category,
+      });
+
+      await categoryRepository.save(categoryExists);
+    }
+
+    const transaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+      category: categoryExists,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    console.log(Object.keys(transaction).length);
+
+    return transaction;
   }
 }
 
